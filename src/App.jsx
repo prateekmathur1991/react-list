d8e9e8fb329fc208ed5539ee171ed9514a840617
Prateek Mathur <prateekmathur1991@gmail.com>
2026-05-17 07:15:56 +0530

    useState implementation - Confirmed working

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
 // This is a React component that serves as the main entry point for the Task Manager application. 
 // It currently renders a simple heading.
 
+import { useState } from "react";
+import TaskItem from "./TaskItem";
+
+const tasksArray = [
+    {id : 1, name: 'Buy Groceries'}, 
+    {id : 2, name : 'Buy Fruits'}, 
+    {id : 3, name : 'Charge Phone'}, 
+    {id : 4, name : 'Complete all tasks'}
+];
+
+
 export default function App() {
+    
+    const [tasks, setTasks] = useState(tasksArray);
+    const [input, setInput] = useState('');
+
     return (
         <>
             <h1>Task Manager</h1>
-            <input type="text" placeholder="Enter a task" />
+            <input type="text" value={input} onChange={(event) => setInput(event.target.value)} placeholder="Enter a task" />
             <button>Add Task</button>
             <ul>
-                <li>Some Random Task</li>
-                <li>Some Random Task</li>
-                <li>Some Random Task</li>
-                <li>Some Random Task</li>
-                <li>Some Random Task</li>
+                {tasksArray.map((task) => (
+                    <TaskItem key={task.id} name={task.name} />
+                ))}
             </ul>
         </>
     );
---
+++ src/TaskItem.jsx
@@ -0,0 +1,5 @@
+export default function TaskItem({id, name }) {
+    return (
+        <li>{name}</li>
+    );
+}
